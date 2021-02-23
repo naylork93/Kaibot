ba5e44538bd8642b583ee0bea91e3726fc0bbe71
Karl Naylor <49456956+naylork93@users.noreply.github.com>
2021-02-23 19:48:40 +0000

    Update quoteofday.py
    Removing commented out sections

--- cogs/quoteofday.py
+++ cogs/quoteofday.py
@@ -17,24 +17,11 @@ class QODCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-#        self.bg_task = self.loop.create_task(self.dailyinspire())
-
     @commands.command(name='inspire')
     async def inspire(self, ctx):
         """Return the Quote of the day"""
         quote = get_quote()
         await ctx.send(quote)
-
-#    async def dailyinspire(self):
-#      """Sends the inspirational quote at 8am daily to a specific channel"""
-#      await self.wait_until_ready()
-#      now = datetime.strftime(datetime.now(),'%H:%M')
-#      channel = client.get_channel(config.INSPIRE_CHANNEL)
-#      while not self.is_closed():
-#        if now == config.INSPIRE_TIME:
-#          quote = get_quote()
-#          await channel.send(quote)
-#          await asyncio.sleep(3600)
 
 def setup(bot):
     bot.add_cog(QODCog(bot))
